40a40cd33f0ce0c9ead3cec2b49a2c2858aca3b1
LJ-Nava <developerluis17@gmail.com>
2026-02-08 18:02:43 -0500

    Fix gh-pages base path to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Project-PlexifyCaps/',
+  base: '/Project-PanaRabbidsCaps/',
   server: {
     port: 5180,
     host: true,
